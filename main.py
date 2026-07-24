0a3b048740d73a825251259ad7572cfcab9f7204
ali4xk <ma964323@gmail.com>
2026-07-24 17:30:28 +0500

    making game logic

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import random
 n = random.randint(1, 100)
-a = int(input("Guess a Number: "))
+a = -1
+guesses = 0
+while(a != n):
+    guesses += 1
+    a = int(input("Guess a Number: "))
+    if a > n:
+        print("Lower Number Please!")
+    else:
+        print("higher Number Please!")
+    
+print("You have guessed the number, Congrats!")
